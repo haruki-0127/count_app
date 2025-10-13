f0652803823a58d0483f94e0c41d9ed088c021a5
haruki-0127 <4649explain@gmail.com>
2025-10-13 19:00:59 +0900

    route.py runtime.py requirements.txt追加

---
+++ app/routes.py
@@ -0,0 +1,58 @@
+from flask import render_template, request
+from app import app
+import re
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = None
+    text_formatted = None
+    warning_message = None
+
+    if request.method == 'POST':
+        if 'text_for_count' in request.form:
+            text = request.form.get('text_for_count','')
+            data = {
+                'whole_length': len(text),
+                'no_space_length': len(re.sub(r'\s+', '', text)),
+                'hiragana': len(re.findall(r'[\u3040-\u309F]', text)),
+                'katakana': len(re.findall(r'[\u30A0-\u30FF]', text)),
+                'kanji': len(re.findall(r'[\u4E00-\u9FFF]', text)),
+                'half_alpha': len(re.findall(r'[A-Za-z]', text)),
+                'full_alpha': len(re.findall(r'[\uFF21-\uFF3A\uFF41-\uFF5A]', text)),
+                'half_num': len(re.findall(r'[0-9]', text)),
+                'full_num': len(re.findall(r'[\uff10-\uff19]', text)),
+                'space': len(re.findall(r'\s+', text)),
+                'word_count': len(re.findall(r"\b[\w']+\b", text))
+            }
+        
+            data['other'] = len(text) - sum(list(data.values())[2:10])
+
+        
+        elif 'text_for_format' in request.form:
+            selected = request.form.get('selected_item')
+            text = request.form.get('text_for_format', '')
+
+            if selected == 'remove':
+                char_remove = request.form.get('char_for_remove', '')
+                text_formatted = text.replace(char_remove, '')
+            
+            elif selected == 'exchange':
+                char_before = request.form.get('char_before', '')
+                char_after = request.form.get('char_after', '')
+                
+                if char_before:
+                    text_formatted = text.replace(char_before, char_after)
+                else:
+                    text_formatted = text
+            
+            elif selected == 'bullet_points':
+                separator = request.form.get('separator', '')
+                text_formatted = '\n'.join('・' + bullet_point for bullet_point in text.split(f'{separator}') if bullet_point)
+            
+            elif selected == 'remove_brank_line':
+                text_formatted = re.sub(r'^\s*$\n', '', text, flags=re.MULTILINE)
+            
+            else:
+                warning_message = 'チェックを入れてください'
+                
+    return render_template('index.html', data=data, text_formatted=text_formatted, warning_message=warning_message)
